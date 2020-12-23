73f72ebeccc37105e22c0049bfa58de460fdb651
Joe Wasden <jpwasden@gmail.com>
2020-12-23 21:08:28 +0000

    File renames

--- mtg_bot.py
+++ mtg_bot.py
@@ -1,9 +1,8 @@
 import os
-import random
-import re
 
 from calculator import calculate
-from dice_roller import roll_dice
+from coin import flip_coin
+from dice import roll_dice
 from discord.ext import commands
 
 
@@ -27,60 +26,7 @@ async def on_message(message):
         await channel.send(reply)
 
     elif message.content.startswith("/flip"):
-        channel = message.channel
-        reply = f"{message.author.mention} "
-
-        if "until" in message.content:
-            fixed_message = message.content.replace("untill", "until")
-            loss_condition = fixed_message.split("until")[1].strip().lower()
-            with_thumb = False
-            proper_command = True
-            if loss_condition not in ["heads", "tails"]:
-                if "with thumb" in loss_condition:
-                    loss_condition = loss_condition.split()[0]
-                    with_thumb = True
-                    if loss_condition not in ["heads", "tails"]:
-                        proper_command = False
-                        reply += "you need to choose 'heads' or 'tails' with 'flip until {}' command."
-                        await channel.send(reply)
-                    else:
-                        loss_condition = loss_condition.split()[0], loss_condition.split()[0]
-                else:
-                    proper_command = False
-                    reply += "you need to choose 'heads' or 'tails' with 'flip until {}' command."
-                    await channel.send(reply)
-
-            if proper_command:
-                wins = 0
-                flip = random.choice(["heads", "tails"])
-                if with_thumb:
-                    flip = flip, random.choice(["heads", "tails"])
-
-                reply += f"\n`{flip}`"
-
-                while flip != loss_condition:
-                    wins += 1
-                    flip = random.choice(["heads", "tails"])
-                    if with_thumb:
-                        flip = flip, random.choice(["heads", "tails"])
-                    reply += f"\n`{flip}`"
-
-                reply += f"\n`wins = {wins}`"
-                await channel.send(reply)
-
-        else:
-            channel = message.channel
-            reply = f"{message.author.mention} "
-
-            try:
-                times = int(re.findall(r"\d+", message.content)[0])
-            except IndexError:
-                times = 1
-
-            for _ in range(times):
-                reply += f"\n`{random.choice(['heads', 'tails'])}`"
-
-            await channel.send(reply)
+        await flip_coin(message)
 
     elif message.content.startswith("/help") or message.content.startswith("/h"):
         channel = message.channel
