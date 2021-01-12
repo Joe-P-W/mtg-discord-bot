8d4d5509db66e7d102c43950e5171f9d22fe0dc9
Joe Wasden <jpwasden@gmail.com>
2021-01-12 17:28:17 +0000

    Updated setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Joe-P-W",
+    name="mtg_discord_bot",
     version="0.0.1",
     author="Joe W",
     author_email="",
     description="An mtg discord bot",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Joe-P-W/mtg-discord-bot",
+    url="https://github.com/Joe-P-W/mtg_discord_bot",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3"
